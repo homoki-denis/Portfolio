48b1919c34150ac7713fdbb62fc308c21c85593e
Denis Homoki <homoki.denisth@gmail.com>
2022-08-01 12:01:32 +0300

    added functionality to nav and sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  let location = useLocation();
+
   return (
     <div className="navbar">
       <Link to="/">
-        <div className="navbar-items navbar-active">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/" && "navbar-active"
+          }`}
+        >
           <img
             src="https://vscode-portfolio.vercel.app/_next/image?url=%2Freact_icon.svg&w=32&q=75"
             alt="react"
@@ -15,7 +21,11 @@ function Navbar() {
       </Link>
 
       <Link to="about">
-        <div className="navbar-items">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/about" && "navbar-active"
+          }`}
+        >
           <img
             src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fhtml_icon.svg&w=32&q=75"
             alt="react"
@@ -25,14 +35,22 @@ function Navbar() {
       </Link>
 
       <Link to="contact">
-        <div className="navbar-items">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/contact" && "navbar-active"
+          }`}
+        >
           <img src="https://img.icons8.com/color/344/sass.png" alt="sass" />
           <span>contact.scss</span>
         </div>
       </Link>
 
       <Link to="projects">
-        <div className="navbar-items">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/projects" && "navbar-active"
+          }`}
+        >
           <img
             src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjs_icon.svg&w=32&q=75"
             alt="javascript"
@@ -42,7 +60,11 @@ function Navbar() {
       </Link>
 
       <Link to="articles">
-        <div className="navbar-items">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/articles" && "navbar-active"
+          }`}
+        >
           <img
             src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjson_icon.svg&w=32&q=75"
             alt="json"
@@ -52,7 +74,11 @@ function Navbar() {
       </Link>
 
       <Link to="github">
-        <div className="navbar-items">
+        <div
+          className={`navbar-items ${
+            location.pathname === "/github" && "navbar-active"
+          }`}
+        >
           <img
             src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fmarkdown_icon.svg&w=32&q=75"
             alt="github"
