9bfbaf5aabf5f55e56c9b6341cee92396fdcb2ab
Denis Homoki <homoki.denisth@gmail.com>
2022-08-01 10:13:26 +0300

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,39 @@ import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 import SecondSideBar from "./components/SecondSideBar";
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Projects from "./pages/Projects";
+import Github from "./pages/Github";
 
 function App() {
   return (
     <div className="App">
-      <TopNav />
+      <BrowserRouter>
+        <TopNav />
 
-      <div className="content">
-        <Sidebar />
-        <SecondSideBar />
-        <div className="main-content">
-          <Navbar />
-          <div>
-            <h1>Home Main Content</h1>
+        <div className="content">
+          <Sidebar />
+          <SecondSideBar />
+          <div className="main-content">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="articles" element={<Articles />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="github" element={<Github />} />
+            </Routes>
           </div>
         </div>
-      </div>
 
-      <Footer />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="navbar-items navbar-active">
-        <img
-          src="https://vscode-portfolio.vercel.app/_next/image?url=%2Freact_icon.svg&w=32&q=75"
-          alt="react"
-        />
-        <span>home.jsx</span>
-      </div>
-      <div className="navbar-items">
-        <img
-          src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fhtml_icon.svg&w=32&q=75"
-          alt="react"
-        />
-        <span>about.html</span>
-      </div>
-      <div className="navbar-items">
-        <img src="https://img.icons8.com/color/344/sass.png" alt="sass" />
-        <span>contact.scss</span>
-      </div>
-      <div className="navbar-items">
-        <img
-          src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjs_icon.svg&w=32&q=75"
-          alt="javascript"
-        />
-        <span>projects.js</span>
-      </div>
-      <div className="navbar-items">
-        <img
-          src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjson_icon.svg&w=32&q=75"
-          alt="json"
-        />
-        <span>articles.json</span>
-      </div>
-      <div className="navbar-items">
-        <img
-          src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fmarkdown_icon.svg&w=32&q=75"
-          alt="github"
-        />
-        <span>github.mb</span>
-      </div>
+      <Link to="/">
+        <div className="navbar-items navbar-active">
+          <img
+            src="https://vscode-portfolio.vercel.app/_next/image?url=%2Freact_icon.svg&w=32&q=75"
+            alt="react"
+          />
+          <span>home.jsx</span>
+        </div>
+      </Link>
+
+      <Link to="about">
+        <div className="navbar-items">
+          <img
+            src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fhtml_icon.svg&w=32&q=75"
+            alt="react"
+          />
+          <span>about.html</span>
+        </div>
+      </Link>
+
+      <Link to="contact">
+        <div className="navbar-items">
+          <img src="https://img.icons8.com/color/344/sass.png" alt="sass" />
+          <span>contact.scss</span>
+        </div>
+      </Link>
+
+      <Link to="projects">
+        <div className="navbar-items">
+          <img
+            src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjs_icon.svg&w=32&q=75"
+            alt="javascript"
+          />
+          <span>projects.js</span>
+        </div>
+      </Link>
+
+      <Link to="articles">
+        <div className="navbar-items">
+          <img
+            src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fjson_icon.svg&w=32&q=75"
+            alt="json"
+          />
+          <span>articles.json</span>
+        </div>
+      </Link>
+
+      <Link to="github">
+        <div className="navbar-items">
+          <img
+            src="https://vscode-portfolio.vercel.app/_next/image?url=%2Fmarkdown_icon.svg&w=32&q=75"
+            alt="github"
+          />
+          <span>github.mb</span>
+        </div>
+      </Link>
     </div>
   );
 }
