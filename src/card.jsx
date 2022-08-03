f1e121a14e4394bbedef6a6bc76f74d1a3fd95c0
Denis Homoki <homoki.denisth@gmail.com>
2022-08-03 22:35:21 +0300

    added content to cards

---
+++ src/card.jsx
@@ -0,0 +1,33 @@
+import blog from "./images/blog.png";
+import meme from "./images/meme.png";
+import product from "./images/product-page.png";
+
+export default [
+  {
+    id: 1,
+    card_title: "My Blog",
+    card_desc: "This is my blog and I love it ",
+    what_use: ["react", "materialUI", "scss"],
+    source_code: "https://github.com/homoki-denis/my-blog",
+    live_demo: "https://my-blog-iota-seven.vercel.app/",
+    img_card: blog,
+  },
+  {
+    id: 2,
+    card_title: "Meme Generator",
+    card_desc: "Something fun I found out, and tried to build it myself",
+    what_use: ["react", "scss"],
+    source_code: "https://github.com/homoki-denis/meme-generator",
+    live_demo: "https://meme-generator-orpin.vercel.app/",
+    img_card: meme,
+  },
+  {
+    id: 3,
+    card_title: "Product Landing Page",
+    card_desc: "One of my first projects",
+    what_use: ["html", "css"],
+    source_code: "https://github.com/homoki-denis/Product-Landing-Page",
+    live_demo: "https://product-landing-page-weld.vercel.app/",
+    img_card: product,
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import card from "../card.json";
+import card from "../card";
 
 function Projects() {
   const cards = card.map((item) => {
     return (
       <div className="projects-card">
-        <img src={item.img_card} alt="" className="project-card-img" />
+        <div className="project-card-img">
+          <img src={item.img_card} alt="" className="card-img" />
+        </div>
         <h3 className="card-title">{item.card_title}</h3>
         <p className="description">{item.card_desc}</p>
         <div className="what-i-use">
