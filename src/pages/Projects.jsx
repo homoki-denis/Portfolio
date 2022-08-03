0981a38fe026e3c08d6461c77eb0274b31f01135
Denis Homoki <homoki.denisth@gmail.com>
2022-08-03 15:58:34 +0300

    added cards on projects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import card from "../card.json";
 
 function Projects() {
+  const cards = card.map((item) => {
+    return (
+      <div className="projects-card">
+        <img src={item.img_card} alt="" className="project-card-img" />
+        <h3 className="card-title">{item.card_title}</h3>
+        <p className="description">{item.card_desc}</p>
+        <div className="what-i-use">
+          <span className="program-used">{item.what_use}</span>
+        </div>
+        <div className="card-footer">
+          <a href={item.source_code}>
+            {" "}
+            <button className="source-code">Source Code</button>
+          </a>
+          <a href={item.live_demo}>
+            {" "}
+            <button className="live-demo">Live Demo</button>
+          </a>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>This is the Projects Page</h1>
+    <div className="projects">
+      <div className="projects-container">
+        <h1>What I've Build So Far</h1>
+        <div className="projects-cards">{cards}</div>
+      </div>
     </div>
   );
 }
